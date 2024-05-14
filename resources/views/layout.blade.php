172cc2cdedea64dd89a8e31b085bc099aba29bdc
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-05-14 11:57:59 +0800

    adjusting header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HRIS</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/navigation/hris-logo.png') }}">
+    <title>{{ ucwords(str_replace('-', ' ', Request::segment(1))) }} | HRIS</title>
     <link rel="stylesheet" href="{{ asset('css/navigation/layout.css') }}">
 </head>
 <body>
