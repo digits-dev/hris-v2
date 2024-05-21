cc10d65035b5e3a9f59f052670ef9446dab3ef1c
joelricmisa <joelricmisa@digits.ph>
2024-05-21 11:28:25 +0800

    add position col to users, add locations table,  rename employees table to employee logs

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,6 +9,8 @@ class Employee extends Model
 {
     use HasFactory;
 
+    protected $table = "employee_logs";
+
     public function scopeSearch($query, $value){
 
         $cleanVal = trim($value);
