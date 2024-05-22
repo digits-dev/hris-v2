44589d51817138c515a35c1387fc1a888e5d7f10
joelricmisa <joelricmisa@digits.ph>
2024-05-22 15:40:43 +0800

    quick update

--- resources/views/modules/employee-attendance/employee-attendance.blade.php
+++ resources/views/modules/employee-attendance/employee-attendance.blade.php
@@ -1,6 +1,6 @@
 @extends("layout")
 @section("content")
-    <p style="margin: 1rem 2.5rem;" class="header-title">{{ ucwords(str_replace('-', ' ', Request::segment(1))) }}</p>
+    <p style="margin: 1rem 2.5rem;" class="header-title">{{ ucwords(str_replace('-', ' ', Request::segment(1))) }} - Summary</p>
 
     @livewire('component.module-contents.employee-attendance.employee-attendance-content')
 @endsection
